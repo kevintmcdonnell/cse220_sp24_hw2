73d945e9cdf3efefba8d7a86c4b4c0152c213608
Kevin McDonnell <44483116+kevintmcdonnell@users.noreply.github.com>
2024-02-13 23:17:46 +0000

    Initial commit

---
+++ src/hw2_main.c
@@ -0,0 +1,16 @@
+#include "hw2.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    return 1000;
+}
